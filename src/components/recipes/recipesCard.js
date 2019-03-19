a8a09170e57b646d80a7325a54357a268ddf6e4a
Paulo <paulo@ingenuity.ph>
2019-03-19 20:19:10 +0800

    Updated recipes page with list view

--- src/components/recipes/recipesCard.js
+++ src/components/recipes/recipesCard.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import { Text, Image } from 'react-native';
-import { Card, Button, Icon } from 'react-native-elements';
+import { Text, Image, View } from 'react-native';
+import { Card, Button, Icon, ListItem } from 'react-native-elements';
 import styles from './styles';
 
 class RecipesCard extends Component {
     render() {
         return (
-            <Card style={{ marginTop: 50}}
-                image={require('../../static/images/banner.jpeg')}>
-                <Text style={{marginBottom: 10, marginTop: 20,}}>
-                    The idea with React Native Elements is more about component structure than actual design.
-                </Text>
-                <Button
-                    backgroundColor='#03A9F4'
-                    buttonStyle={styles.buttonStyle}
-                    title='Check Tutorial' />
+            <Card containerStyle={{ padding: 0 }} style={{ marginTop: 50, height: 600}}>
+                <ListItem 
+                    style={{ height: 100 }}
+                    title="Matcha Cake" 
+                    subtitle={
+                        <View style={{ height: 50 }}>
+                            <Text>Matcha chocolate</Text>
+                            <Button
+                                title="Clear button"
+                            />
+                        </View>
+                    }
+                    leftAvatar={{ source: { uri: 'https://picsum.photos/g/100/' }}} />
+                <ListItem 
+                    style={{ height: 100 }}
+                    title="Matcha Cake" 
+                    subtitle={
+                        <Button
+                            title="Clear button"
+                        />
+                    }
+                    leftAvatar={{ uri: 'https://picsum.photos/g/100/' }} />
+                <ListItem 
+                    style={{ height: 100 }}
+                    title="Matcha Cake" 
+                    subtitle={
+                        <Button
+                            title="Clear button"
+                        />
+                    }
+                    leftAvatar={{ uri: 'https://picsum.photos/g/100/' }} />
             </Card>
         );
     }
