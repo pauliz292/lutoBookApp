e1321ae1482904840e120ea9c09bb0a4c1a019c4
Paulo <paulo@ingenuity.ph>
2019-04-16 14:39:04 +0800

    Added get location to location component

--- src/components/locationService/marketLocation.js
+++ src/components/locationService/marketLocation.js
@@ -4,13 +4,30 @@ import { WebView } from 'react-native-webview';
 import BackButton from '../_common/back';
 
 class LocationPage extends Component {
+    componentDidMount() {
+        // let geoOptions = {
+        //     enableHighAccuracy: true,
+        //     timeOut: 20000,
+        //     maximumAge: 60 * 60 * 24
+        // };
+        navigator.geolocation.getCurrentPosition(this.geoSuccess, this.geoError);
+    }
+
+    geoSuccess = position => {
+        console.log(position.coords.longitude, position.coords.latitude);
+    };
+
+    geoError = err => {
+        console.log(err.message);
+    };
+
     render() {
         return (
             <View style={styles.wrapper}>
                 <View style={{ height: 30 }}>
                     <BackButton />
                 </View>
-                <WebView source={{ uri: 'https://www.google.com/maps/' }} />
+                {/* <WebView source={{ uri: 'https://www.google.com/maps/' }} /> */}
             </View>
         );
     }
