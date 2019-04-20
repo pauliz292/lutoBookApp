8aad7f883a98c2ee7fb059fb0159be166f70d161
Paulo <paulo@ingenuity.ph>
2019-04-20 19:34:14 +0800

    Installed google places

--- android/app/src/main/java/com/lutobookapp/MainApplication.java
+++ android/app/src/main/java/com/lutobookapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.lutobookapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.rnfs.RNFSPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGooglePlacesPackage(),
             new ImageResizerPackage(),
             new ImagePickerPackage(),
             new RNFSPackage(),
